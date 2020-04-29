0bdcbb841bb5ac2070a85a0dd54b25416babf444
David Allen <trooper898@gmail.com>
2020-04-29 08:24:03 -0400

    removed unused shell scripts

--- lstime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for dir in */; do
-    timestamp=$(find ./$dir -type f -printf "%T@ %t\\n" | sort -nr -k 1,2 | head -n 1)
-    printf "%s %s\n" "$timestamp" "$dir"
-done | sort -nr -k 1,2 | awk '{$1=""; print}'
